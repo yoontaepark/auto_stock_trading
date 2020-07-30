f04df7a074baa5538366abe15d761a265966a9bc
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-07-30 23:14:04 +0900

    int.py
    recap_int

---
+++ basic/int.py
@@ -0,0 +1,25 @@
+#int 더하기
+stock_price = 2000
+stock_price2 = 2500
+stock_plus = stock_price + stock_price2
+print(stock_plus)
+
+#int 빼기
+stock_minus = stock_price - stock_price2
+print(stock_minus)
+
+#int 곱하기
+stock_multi = stock_price * stock_price2
+print(stock_multi)
+
+#int 나누기
+stock_div = stock_price / stock_price2  #결과는 float
+stock_div2 = stock_price // stock_price2 #소수점 버림, int형이 됨
+print(stock_div)
+print(type(stock_div))
+print(stock_div2)
+print(type(stock_div2))
+
+#int 나눈 나머지값
+stock_share = stock_price2 % stock_price
+print(stock_share)
